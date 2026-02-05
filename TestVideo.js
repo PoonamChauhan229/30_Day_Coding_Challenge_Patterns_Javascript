9008d4a2547906c008f05b42fd4d2cf25f096af2
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2026-02-05 21:12:43 +0530

    23 pattern

--- TestVideo.js
+++ TestVideo.js
@@ -34,32 +34,31 @@
 // Inner Loop-2 >> Manage n no. of cols >> j loop >> Print star
 
 
-
 // 1234567 >> cols >> j loop
 //    *        >>1 >>i loop  >>Print 1 star & Print 3 spaces
 //   ***       >>2 >>i loop  >>Print 3 stars & Print 2 spaces
 //  *****      >>3 >>i loop  >>Print 5 stars & Print 1 space
 // *******     >>4 >>i loop  >>Print 7 stars & Print 0 spaces
 
-//  *****      >>1 >>i loop  >>Print 5 stars & Print 1 space
-//   ***       >>2 >>i loop  >>Print 3 stars & Print 2 spaces
-//    *        >>3 >>i loop  >>Print 1 star  & Print 3 spaces
+//  12345 >> cols >> j loop
+//  *****      >>2 >>i loop  >>Print 5 stars & Print 1 space
+//   ***       >>3 >>i loop  >>Print 3 stars & Print 2 spaces
+//    *        >>4 >>i loop  >>Print 1 star  & Print 3 spaces
 
 // Upper Pyramid
-// i loop >>start-1 ||end-n ||n=4
-// k loop >>start-1 ||end-n-i ||i=1,3||i=2,2
-// j loop >>start-1 ||end-(2*i)-1
-// (2*i)-1>>i=1 >>(2*1)-1=2-1>>1 || i=2 ,(2*2)-1>>4-1=3
+// i loop >>start-1 ||end-n ||n=4                      >>i=1;i<=n;i++
+// k loop >>start-1 ||end-n-i ||i=1,3||i=2,2           >>k=1;k<=n-i;k++
+// j loop >>start-1 ||end-(2*i)-1 || i=1,1 | i=2,3 |i=3,5 |i=4,7 >>j=1;j<=(2*i)-1;j++
 
 // Lower Inverted Pyramid
-// i loop >>start-2 ||end-n ||n=4
-// k loop >>start-1 ||end-i-1 ||i=1>0,i=2>1,i=3>2,i=4>3 >>k=1;k<=i-1;k++
-// j loop >>start-1 ||end-2*(n-i)+1 >>i=1>>7,i=2>>5,i=3>>3,i=4>>1
+// i loop >>start-2 ||end-n ||n=4                   >>i=2;i<=n;i++
+// k loop >>start-1 ||end-i-1 ||i=2>1,i=3>2,i=4>3   >>k=1;k<=i-1;k++
+// j loop >>start-1 ||end-2*(n-i)+1 >>i=2>>5,i=3>>3,i=4>>1  >>j=1;j<=2*(n-i)+1;j++
 
 // Working:
 // For detailed working and dry-run explanation,
 // please refer to Video 17 and Video 20 of this pattern series.
-// 1 change >> Lower Inverted Pyramid >>i=2
+// 1 change >> Lower Inverted Pyramid >>i=2 so you can just omit what happens when i=1, rest remains the same
 
 // Code Implementation
 
